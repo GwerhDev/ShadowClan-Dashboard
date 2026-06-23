8bf5dd9e29aeb8e657c5dd6c3cec37f852adbfab
gerardo <gerardo@recemed.com>
2026-06-23 09:04:58 -0400

    chore: migrate URLs to VITE env vars, ignore .env files

--- src/middlewares/misc/const.ts
+++ src/middlewares/misc/const.ts
@@ -1,8 +1,6 @@
-import { environment } from '../../environment';
-
-export const API_URL: string    = environment === 'development' ? 'http://localhost:8080'  : 'https://api.shadowclan.cl';
-export const WEB_URL: string    = environment === 'development' ? 'http://localhost:5173'  : 'https://shadowclan.cl';
-export const CLIENT_URL: string = environment === 'development' ? 'http://localhost:5174'  : 'https://app.shadowclan.cl';
+export const API_URL: string    = import.meta.env.VITE_API_URL;
+export const WEB_URL: string    = import.meta.env.VITE_WEB_URL;
+export const CLIENT_URL: string = import.meta.env.VITE_CLIENT_URL;
 
 export const classes = [
   { name: "Druida", value: "druid", image: "https://blz-contentstack-images.akamaized.net/v3/assets/blt77f4425de611b362/blt75eca7075dbe5195/6851ec39300f1cc6e7953c4b/druid_svg_1.svg" },
